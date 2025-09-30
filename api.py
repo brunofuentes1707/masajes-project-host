d5740ca3ea334908fe204f780adae2030bc15e61
brunofuentes1707 <brunofuentes1707@gmail.com>
2025-09-30 00:17:52 -0300

    Update api.py
    Switch to SSL/465 for SMTP to avoid Render timeout

--- api.py
+++ api.py
@@ -12,8 +12,8 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # --- CONFIGURACIÓN PARA FLASK-MAIL ---
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
